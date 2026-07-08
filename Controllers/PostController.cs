1511898e399028fb3e4b2fe310676aa7c45bd618
Kris Cagle <Kriscagle@gmail.com>
2026-07-08 09:35:09 -0500

    added ViewAllPosts endpoint

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -6,6 +6,7 @@ using Tabloid.Models.DTOs;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace Tabloid.Controllers;
 
@@ -23,5 +24,24 @@ public class TabloidController : ControllerBase
     }
 
     // Add your controllers here
-    
+[HttpGet("posts")]
+    public IActionResult GetPosts()
+    {
+        List<Post> posts = _dbContext.Posts
+            .Include(p => p.UserProfile)
+            .Where(p => p.PublicationDate <= DateTime.Now)
+            .OrderByDescending(p => p.PublicationDate)
+            .ToList();
+
+        List<PostDTO> postDTOs = _mapper.Map<List<PostDTO>>(posts);
+
+        return Ok(postDTOs);
+    }
+
+
+
+
 }
+
+
+
